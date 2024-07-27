857f79eda19577268b4e91e132d9c9949a0a24f4
qwerzl <me@qwerzl.me>
2024-07-27 13:04:57 +0800

    feat: orbit control

--- pages/game.vue
+++ pages/game.vue
@@ -4,6 +4,7 @@
 <template>
   <TresCanvas clear-color="#FFFFFF" window-size>
     <TresPerspectiveCamera />
+    <OrbitControls />
     <TresMesh>
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
       <TresMeshBasicMaterial color="white" />
